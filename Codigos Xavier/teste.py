70a78361ed648880ed18518156cf801308b33017
Gustavo Xavier <gustavoxavier7878@gmail.com>
2025-08-28 15:52:22 -0300

    salvando codigos no github

---
+++ Codigos Xavier/teste.py
@@ -0,0 +1,54 @@
+#print("Gustavo"*8000)
+#exemplo de variavel variavel do tipo texto
+fruta = "manga"
+print(type(fruta))
+#Exemplo de variavel do tipo INT é inteiro, numeros inteiros
+idade = int(17)
+# Exemplo de numero  quebrado, float
+nota = float(9.5)
+
+#booleana
+vc_curte = False
+
+### EXERCICIO
+
+#1 criar uma variavel que peça o nome do usuario.
+#2 criar uma variavel do tipo float, que pede a nota do usuario
+#3 depois criar uma variavel que peça a idade do usuario
+#4 depois  de um print nas variaveis
+
+# 1
+
+nome = input("digite seu nome")
+
+#2 
+ 
+nota = float(input("digite sua nota"))
+
+#3
+
+idade = int(input("digite sua idade"))
+
+print(nome)
+
+print (nota)
+
+print (idade)
+
+### exercicio 2
+
+## crie uma vbariavel que se chama nome_carro, coloque dentro dessa variavel  o noem do seu carro dos sonhos 
+## crie uma varavel que se chama preço, coloque dentro dessa variavel o preço do carro
+##crie uma varavel que se chma ano, coloque  dentro dessa variavel  o ano do carro
+
+nome_carro  = "Lamborghini Urus"
+ 
+preço = float(4.0205000)
+
+ano = int(2025)
+ 
+print(nome_carro)
+
+print(preço)
+
+print(ano)
