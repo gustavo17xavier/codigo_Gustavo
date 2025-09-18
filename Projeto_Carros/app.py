0adde0bd4983a521a70dae2e549131115fcefd99
Gustavo Xavier <gustavoxavier7878@gmail.com>
2025-09-18 16:30:34 -0300

    Add files via upload
    LOJA ALUGUEL DE CARRO

--- Projeto_Carros/app.py
+++ Projeto_Carros/app.py
@@ -1,16 +1,40 @@
 import streamlit as st
-##coloca um titulo
+st.sidebar.title("XAVIER EMPIRE")
+st.sidebar.image("logo.png")
 
-st. title("titulo da loja")
+carros = ['Bugatti Chiron Super Sport','Pagani Huayra R','McLaren Speedtail','Aston Martin Valkyrie']
 
-###escreve
-st.write("testando...esquedar e direita")
+opçao = st.sidebar.selectbox('Escolha o carro que foi alugado', carros)
 
-###cria uma barra lateral
-st.sidebar.title("barra lateral")
+st.title('XAVIER EMPIRE - Aluguel de Carros de Luxo✨🏎️')
 
-## criando  a lista
-nomes =["uno", "chevette", "mclaren","bugatti"]
+st.image(f'{opçao}.png')
+st.markdown(f'## Você alugou o modelo de luxo:  {opçao}')
+st.markdown('---')
 
-    ###cria uma caixa na barra lateral
-st.sidebar.selectbox("selecione um nome: " , nomes)
+dias = st.text_input(f'Por quantos dias o {opçao}foi alugado?')
+km = st.text_input(f'Quantos km você rodou com o {opçao}?')
+## diaria dos carros
+if opçao == 'Bugatti Chiron Super Sport':
+    diaria = 50000
+
+elif opçao == 'Pagani Huayra R':
+    diaria = 65000
+
+elif opçao == 'McLaren Speedtail':
+    diaria = 78000
+
+elif opçao == 'Aston Martin Valkyrie':
+    diaria = 89000
+
+### calcular
+if st.button('calcular'):
+    dias = int(dias)
+    km = float(km)
+
+
+    total_dias = dias * diaria
+    total_km = km * 0.15
+    aluguel_total = total_dias + total_km
+
+    st.warning(f'Você alugou o {opçao} por {dias} dias e rodou {km}km. O valor total a pagar é R$:{aluguel_total:.2f}')
---
+++ Projeto_Carros/teste.py
@@ -0,0 +1,18 @@
+import streamlit as st
+##coloca um titulo
+
+st. title("titulo da loja")
+
+###escreve
+st.write("testando...esquedar e direita")
+
+###cria uma barra lateral
+st.sidebar.title("barra lateral")
+
+## criando  a lista
+nomes =["uno", "chevette", "mclaren","bugatti"]
+
+    ###cria uma caixa na barra lateral
+st.sidebar.selectbox("selecione um nome: " , nomes)
+### coloca video
+st.video ("https://www.youtube.com/watch?v=kbHoE6jU9ZQ")
